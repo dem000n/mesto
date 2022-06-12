342e79e3b7976621d3fd181042958c70cd7d94c8
dem000n <rghti4635@gmail.com>
2022-06-12 10:43:14 +0300

    popup logic added

--- script/index.js
+++ script/index.js
@@ -1,10 +1,31 @@
 const editProfileBtn = document.querySelector('.profile__edit-btn')
+const closeProfilePopupBtn = document.querySelector('.popup__close-btn')
 const popup = document.querySelector('.popup')
-// console.log(editProfileBtn)
+const popupSubmitBtn = document.querySelector('.popup__save-btn')
+
+let popupNameText = document.querySelector('.profile__name').textContent
+document.querySelector('.popup__name').value = popupNameText
+
+let popupAboutText = document.querySelector('.profile__about').textContent
+document.querySelector('.popup__about').value = popupAboutText
+
+
+
+editProfileBtn.addEventListener('click', showEditProfilePopup)
+closeProfilePopupBtn.addEventListener('click', hideEditProfilePopup)
+popupSubmitBtn.addEventListener('click', popupSubmitHandler)
 
 function showEditProfilePopup() {
     popup.classList.add('popup_opened')
-    console.log(editProfileBtn)
 }
 
-editProfileBtn.addEventListener('click', showEditProfilePopup)
+function hideEditProfilePopup() {
+    popup.classList.remove('popup_opened')
+}
+
+function popupSubmitHandler(evt) {
+    evt.preventDefault()
+    document.querySelector('.profile__name').childNodes[0].nodeValue = document.querySelector('.popup__name').value
+    document.querySelector('.profile__about').textContent = document.querySelector('.popup__about').value
+    popup.classList.remove('popup_opened')
+}
