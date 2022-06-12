059bc9fde4d34c815b785813a25f25c0b484278f
dem000n <rghti4635@gmail.com>
2022-06-12 11:23:36 +0300

    added like btn active state

--- script/index.js
+++ script/index.js
@@ -3,17 +3,17 @@ const closeProfilePopupBtn = document.querySelector('.popup__close-btn')
 const popup = document.querySelector('.popup')
 const popupSubmitBtn = document.querySelector('.popup__save-btn')
 
+
 let popupNameText = document.querySelector('.profile__name').textContent
 document.querySelector('.popup__name').value = popupNameText
 
 let popupAboutText = document.querySelector('.profile__about').textContent
 document.querySelector('.popup__about').value = popupAboutText
 
-
-
 editProfileBtn.addEventListener('click', showEditProfilePopup)
 closeProfilePopupBtn.addEventListener('click', hideEditProfilePopup)
 popupSubmitBtn.addEventListener('click', popupSubmitHandler)
+
 
 function showEditProfilePopup() {
     popup.classList.add('popup_opened')
@@ -29,3 +29,4 @@ function popupSubmitHandler(evt) {
     document.querySelector('.profile__about').textContent = document.querySelector('.popup__about').value
     popup.classList.remove('popup_opened')
 }
+
