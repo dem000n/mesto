af9fe2b27ff070dad8917e8ccc48892d1f49d533
dem000n <rghti4635@gmail.com>
2022-06-25 12:42:44 +0300

    add dynamic cards loading function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 const editProfileBtn = document.querySelector('.profile__edit-btn')
 const closeProfilePopupBtn = document.querySelector('.popup__close-btn')
 const popup = document.querySelector('.popup')
@@ -15,7 +41,7 @@ form.addEventListener('submit', popupSubmitHandler)
 
 function showEditProfilePopup() {
     popupNameText.value = profileNameText.textContent
-    popupAboutText.value =  profileAboutText.textContent
+    popupAboutText.value = profileAboutText.textContent
     popup.classList.add('popup_opened')
 }
 
@@ -30,3 +56,18 @@ function popupSubmitHandler(evt) {
     hideEditProfilePopup()
 }
 
+
+
+function loadInitialCards() {
+    const placeCardTemplate = document.querySelector('#place-card-template').content
+
+    initialCards.forEach((card) => {
+        const placeCardElement = placeCardTemplate.querySelector('.place-card').cloneNode(true)
+        const placeCards = document.querySelector('.places-grid')
+        placeCardElement.querySelector('.place-card__image').src = card.link
+        placeCardElement.querySelector('.place-card__title').textContent = card.name
+        placeCards.append(placeCardElement);
+    })
+}
+
+loadInitialCards()
